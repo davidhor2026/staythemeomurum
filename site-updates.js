637a94fc51bf142d9cbfd19e31bcdb46b1003603
davidhor2026 <davidhor74@gmail.com>
2026-07-22 17:44:54 +0900

    Add files via upload

--- site-updates.js
+++ site-updates.js
@@ -6,7 +6,7 @@ window.SITE_UPDATES = {
    * 하위 페이지가 갱신되면 groups의 상위 메뉴에도 NEW가 자동 전파됩니다.
    */
   pages: {
-    'index.html': { key: 'home', updated: '2026-07-21', expires: '2026-08-02' },
+    'index.html': { key: 'home', updated: '2026-07-22', expires: '2026-08-16' },
     'rooms.html': { key: 'rooms', updated: '' },
     'facilities.html': { key: 'facilities', updated: '' },
     'reviews.html': { key: 'reviews', updated: '' },
@@ -16,8 +16,8 @@ window.SITE_UPDATES = {
     'psat-ncs-quiz.html': { key: 'psatNcsQuiz', updated: '2026-07-15' },
     'korean-history-advanced-quiz.html': { key: 'historyQuiz', updated: '2026-07-15' },
     'foreign-service-interview-quiz.html': { key: 'foreignInterviewQuiz', updated: '2026-07-15' },
-    'exam-insight.html': { key: 'examInsight', updated: '2026-07-20', expires: '2026-07-28' },
-    'exam-schedule.html': { key: 'examSchedule', updated: '2026-07-21', expires: '2026-08-02' },
+    'exam-insight.html': { key: 'examInsight', updated: '2026-07-22', expires: '2026-08-16' },
+    'exam-schedule.html': { key: 'examSchedule', updated: '2026-07-22', expires: '2026-08-16' },
     'games.html': { key: 'games', updated: '' },
     'fortune.html': { key: 'fortune', updated: '' },
     'knowledge.html': { key: 'knowledge', updated: '' },
@@ -149,3 +149,5 @@ window.SITE_UPDATES = {
 // local7-registration-20260720
 
 // police-superintendent-76-exam-venue-20260721
+
+// seoul-education-9-pass-interview-20260722
